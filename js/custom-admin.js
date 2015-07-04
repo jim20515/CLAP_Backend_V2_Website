dc1905339c636687fd5f67579fcd5a059b65f7c1
Jim <jim@whatsmedia.com>
2015-07-05 01:40:14 +0800

    Web api

---
+++ js/custom-admin.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    $('select').selectpicker();
+
+    $('.date').datepicker({
+        language: "zh-TW",
+        clearBtn: true,
+        todayHighlight: true,
+        format: "yyyy-mm-dd",
+        autoclose: true
+    });
+
+    $('.radio :radio, .checkbox :checkbox').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue',
+        increaseArea: '20%' // optional
+    });
+
+    // Fixed bootstrap validation class
+    $("span.field-validation-valid, span.field-validation-error").addClass('help-block');
+    $("div.form-group").has("span.field-validation-error").addClass('has-error');
+    $("div.validation-summary-errors").has("li:visible").addClass("alert alert-block alert-danger");
+});
---
+++ js/table-admin-x-editable.js
@@ -0,0 +1,12 @@
+﻿var table = $('#example').dataTable({
+    //columnDefs: [{
+    //    targets: [0],
+    //    visible: false,
+    //    searchable: false
+    //}],
+    language: { url: "/Scripts/DataTables/Chinese-traditional.json" },
+    //stateSave: true,
+    //scrollX: true,
+    lengthMenu: [[5, 10, 20, 50, -1], [5, 10, 20, 50, "全部"]],
+    iDisplayLength: 10
+});
